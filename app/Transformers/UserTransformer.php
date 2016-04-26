d946b416b570a1b52e18acd8bae5314c3dfa66a7
lk22 <nightskater-220@hotmail.com>
2016-04-27 01:04:44 +0200

    setup user transformer

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,24 @@
+<?php 
+	namespace Blooddivision\Transformers;
+
+	class UserTransformer extends Transformer {
+
+		public function transform($user){
+
+			$return = [
+				'id' 		=> (integer) $user->id,
+				'username' 	=> (string) $user->name,
+				'email' 	=> (string) $user->email,
+				'active'	=> (boolean) $user->active,
+
+				'isguest'	=> $user->isGuest()
+			];
+
+			return $return;
+
+		}
+
+	}
+
+
+ ?>

cf514805baaf6a353e3b6238ac5854183bda2f25
lk22 <nightskater-220@hotmail.com>
2016-04-27 00:59:17 +0200

    setup transformer class

---
+++ app/Transformers/Transformer.php
@@ -0,0 +1,21 @@
+<?php 
+	namespace Blooddivision\Transformers;
+
+	abstract class Transformer {
+
+		public function transformCollection($items) {
+
+			$return = [];
+
+			foreach($items as $item)
+				$return[] = $this->tranform($item);
+
+			return $return;
+
+			return array_map([$this, $this->tranform], $items);
+
+		}
+
+		public abstract function transform($item);
+	}
+ ?>
